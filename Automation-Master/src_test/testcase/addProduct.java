36ae1a49cdc0fd53c7a024a341b57f020b5597c9
prasetyo putra <prasetyo531@gmail.com>
2018-05-15 10:51:49 +0700

    akus

---
+++ Automation-Master/src_test/testcase/addProduct.java
@@ -0,0 +1,87 @@
+package testcase;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Date;
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.logging.LogEntries;
+import org.openqa.selenium.logging.LogEntry;
+import org.openqa.selenium.logging.LogType;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+import jxl.Cell;
+import jxl.Sheet;
+import jxl.Workbook;
+import pageObjects.AssertElement;
+import pageObjects.CartPage;
+import pageObjects.CategoryPage;
+import pageObjects.CheckoutPage;
+import pageObjects.ProductPage;
+import resources.controller;
+import resources.support;
+
+public class addProduct extends controller {
+	
+public static Logger log =LogManager.getLogger(support.class.getName());
+	
+	public static RemoteWebDriver driver= null;
+	public static WebElement main= null;
+	public static Properties prop=null;
+	
+	@BeforeTest
+	@Parameters({ "browser" })
+	public void setUp(String browser) throws IOException {
+		System.out.println("*******************");
+		driver = controller.getDriver(browser);
+		
+	}
+	
+	@Test(dataProvider="existingCust2")
+	public void scenario_satu(String email,String password,String alamat,String telepon) throws Exception {
+		
+		support supp= new support();
+		AssertElement asser = new AssertElement(driver);
+		CategoryPage cat = new CategoryPage(driver);
+		ProductPage prod = new ProductPage(driver);
+		CartPage cpage = new CartPage(driver);
+		CheckoutPage checkout = new CheckoutPage(driver);
+		
+		prop= new Properties();
+		FileInputStream fis=new FileInputStream("//Users//mac//Documents//Automation//mavenjob//Automation-Master//src_controller//resources//data.properties");
+		prop.load(fis);
+		String testenv=prop.getProperty("testlocation");
+		
+		if(testenv.equalsIgnoreCase("prod")){
+        	driver.navigate().to("http://femaledaily.com/");  //https://dev.uangteman.com/a/NHeHv
+             driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        	} else {
+        		driver.navigate().to("http://femaledaily.net/");  //https://dev.uangteman.com/a/NHeHv
+                driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        	}
+		
+		driver.manage().window().maximize();
+		String strPageTitle = driver.getTitle();
+		System.out.println(strPageTitle);
+	
+		
+		
+		
+		
+		
+	}
+
+}
