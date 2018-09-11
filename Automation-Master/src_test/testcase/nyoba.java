64ebf2493f1a307e1f5a3da07e14f33b9bdd367c
prasetyo putra <prasetyo531@gmail.com>
2018-09-11 15:08:48 +0700

    addcoobacoba

---
+++ Automation-Master/src_test/testcase/nyoba.java
@@ -0,0 +1,180 @@
+package testcase;
+
+import static org.testng.Assert.assertTrue;
+
+import java.awt.Robot;
+import java.awt.Toolkit;
+import java.awt.datatransfer.StringSelection;
+import java.awt.event.KeyEvent;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Date;
+import java.util.List;
+import java.util.Properties;
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
+
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.logging.LogEntries;
+import org.openqa.selenium.logging.LogEntry;
+import org.openqa.selenium.logging.LogType;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+import jxl.Cell;
+import jxl.Sheet;
+import jxl.Workbook;
+import pageObjects.AssertElement;
+import pageObjects.CartPage;
+import pageObjects.CategoryPage;
+import pageObjects.CheckoutPage;
+import pageObjects.ProductPage;
+import pageObjects.addproductpage;
+import pageObjects.homepage;
+import pageObjects.login;
+import pageObjects.productdetail;
+import pageObjects.productlist;
+import resources.controller;
+import resources.support;
+import resources.ConnectDB;
+
+public class nyoba extends controller {
+	
+	public static RemoteWebDriver driver= null;
+	public static WebElement main= null;
+	public static Properties prop=null;
+	
+	public String UrlLogin = null;
+	public String UrlPageDetail = null;
+	
+	private static final char[] characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".toCharArray();
+	private static final Random random = new Random(new Date().getTime());
+	
+	private static String createRandomString(int length) {
+        final StringBuffer stringBuffer = new StringBuffer();
+        for (int i = 0; i < length; i++) {
+            String randomValue;
+            if (random.nextInt(2) == 1) {
+                randomValue = String.valueOf(random.nextInt(10));
+            } else {
+                randomValue = Character.toString(characters[random.nextInt(26)]);
+            }
+            stringBuffer.append(randomValue);
+        }
+        return stringBuffer.toString();
+    }
+
+	@BeforeTest
+	@Parameters({ "browser" })
+	public void setUp(String browser) throws IOException {
+		System.out.println("*******************");
+		driver = controller.getDriver(browser);
+		
+	}
+	
+	@Test(dataProvider="existingCust")
+	public void scenario_satu(String email,String password,String alamat,String telepon) throws Exception {
+		
+		support supp= new support();
+		homepage home = new homepage(driver);
+		login logpro = new login(driver);
+		addproductpage productpage = new addproductpage(driver);
+		productlist prodlist = new productlist(driver);
+		productdetail proddet = new productdetail(driver);
+		
+		AssertElement asser = new AssertElement(driver);
+		CategoryPage cat = new CategoryPage(driver);
+		ProductPage prod = new ProductPage(driver);
+		CartPage cpage = new CartPage(driver);
+		CheckoutPage checkout = new CheckoutPage(driver);
+		
+		prop= new Properties();
+		FileInputStream fis=new FileInputStream("//Users//mac//Documents//Automation//mavenjob//Automation-Master//src_controller//resources//data.properties");
+		prop.load(fis);
+		String testenv=prop.getProperty("testlocation");
+		
+		if(testenv.equalsIgnoreCase("prod")){
+        	driver.navigate().to("https://www.phptravels.net/");  //https://dev.uangteman.com/a/NHeHv
+             driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        	} else {
+        		driver.navigate().to("https://www.phptravels.net/");  //https://dev.uangteman.com/a/NHeHv
+                driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        	}
+		
+	
+		driver.manage().window().maximize();
+		String strPageTitle = driver.getTitle();
+		System.out.println(strPageTitle);
+		
+		driver.findElement(By.cssSelector("input[class='form-control input-lg dpd1'")).sendKeys("05/09/2018");
+		driver.findElement(By.cssSelector("input[class='form-control input-lg dpd2'")).sendKeys("08/09/2018");
+		
+		driver.findElement(By.cssSelector("button[class='btn btn-lg btn-block btn-danger pfb0 loader'")).click();
+		
+
+		List<WebElement> radiobtn = driver.findElementsByCssSelector("div[class='hpadding20'");
+		System.out.println(radiobtn.size());
+		radiobtn.get(new Random().nextInt(radiobtn.size())).click();
+       
+	}
+	
+	@AfterMethod
+	public void tearDown() {
+		if(driver!=null) {
+			System.out.println("Closing browser");
+//			driver.close();
+		}
+	}
+	
+	public void ExtractJSLogs() {
+        LogEntries logEntries = driver.manage().logs().get(LogType.BROWSER);
+        for (LogEntry entry : logEntries) {
+            System.out.println(new Date(entry.getTimestamp()) + " " + entry.getLevel() + " " + entry.getMessage());
+        }
+    }
+	
+	@DataProvider	  
+	public Object[][] existingCust() throws Exception {
+	     
+		FileInputStream filepath = new FileInputStream("//Users//mac//Documents//Automation//mavenjob//Automation-Master//Workbook1.xls");
+
+		Workbook wb = Workbook.getWorkbook(filepath);
+		Sheet sheet = wb.getSheet("existing");
+
+		int row = sheet.getRows();
+		System.out.println("number of rows"+row);
+		int column = sheet.getColumns();
+		System.out.println("number of columns"+column);
+		String Testdata[][] = new String[row-1][column];
+		int count=0;
+
+		     for (int i = 1; i < row; i++)
+		     	{
+		    	 for (int j = 0; j < column; j++)
+		    	 {
+		    		 Cell cell = sheet.getCell(j, i);
+		    		 Testdata[count][j] = cell.getContents();
+		     	}
+		    	 count++;
+		       }
+		     filepath.close();
+		     return Testdata;
+		     }
+	
+	
+}
